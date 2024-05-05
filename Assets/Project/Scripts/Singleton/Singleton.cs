e8aa6bfab0c70f1b1ff1987d98a96a9d48abcf7e
Araib <araib.saleem@gmail.com>
2024-05-05 16:00:49 -0400

    Project organization

---
+++ Assets/Project/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Project.Game.Singleton
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T m_Instance;
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = (T)FindFirstObjectByType(typeof(T));
+                }
+                return m_Instance;
+            }
+        }
+
+        void Awake()
+        {
+            if (m_Instance != null && m_Instance != this as T)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                m_Instance = this as T;
+            }
+        }
+    }
+}
